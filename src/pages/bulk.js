dc49ddb3131cb95ff702a4fb3272ee52f8c20699
Santiago Sipoli <santiago.wd@gmail.com>
2023-02-15 13:15:56 -0300

    Create bulk

---
+++ src/pages/bulk.js
@@ -0,0 +1,14 @@
+import { Container, Flex, Heading } from '@chakra-ui/react'
+import Head from 'next/head'
+
+export default function Bulk() {
+	return (
+		<>
+			<Head>
+				<title>Leads Search - Santiago</title>
+			</Head>
+
+			<Flex direction="row"></Flex>
+		</>
+	)
+}
