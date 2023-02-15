9b866c187ff266e0784e7ec0050262b12b8124a5
Santiago Sipoli <santiago.wd@gmail.com>
2023-02-15 11:49:33 -0300

    Improve listing Add copy to clipboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import {
+	useToast,
 	Container,
 	Flex,
 	Box,
@@ -9,18 +10,127 @@ import {
 	Divider,
 	Input,
 	Button,
+	IconButton,
 	FormControl,
 	FormLabel,
 	FormHelperText,
 } from '@chakra-ui/react'
 
+import { CopyIcon } from '@chakra-ui/icons'
+
+function Consulta({ data }) {
+	const toast = useToast()
+	function clickCopy(text, label) {
+		navigator.clipboard.writeText(text)
+		toast({
+			title: `${label} copiado!`,
+			isClosable: true,
+			duration: 1500,
+			position: 'top-right',
+		})
+	}
+
+	const { razao_social, estabelecimento: empresa } = data
+
+	const parse = [
+		{
+			label: 'Situação Cadastral',
+			value: empresa.situacao_cadastral,
+			bg:
+				empresa.situacao_cadastral !== 'Ativa'
+					? 'red.100'
+					: 'green.100',
+		},
+		{
+			label: 'Razão Social',
+			value: razao_social,
+		},
+		{
+			label: 'Nome Fantasia',
+			value: empresa.nome_fantasia,
+		},
+		{
+			label: 'Cidade - UF',
+			value: `${empresa.cidade.nome} - ${empresa.estado.sigla}`,
+		},
+		{
+			label: 'E-mail',
+			value: empresa.email,
+		},
+		{
+			label: 'Telefones',
+			value: `(${empresa.ddd1}) ${empresa.telefone1.replace(
+				/(.{4}$)/,
+				'-$1'
+			)} ${
+				data.estabelecimento.ddd2 !== null
+					? ` / (${
+							data.estabelecimento.ddd2
+					  }) ${data.estabelecimento.telefone2.replace(
+							/(.{4}$)/,
+							'-$1'
+					  )}`
+					: ''
+			}`,
+		},
+		{
+			label: 'Endereço',
+			value: `${empresa.tipo_logradouro} ${empresa.logradouro}, ${empresa.numero}, ${empresa.cidade.nome}`,
+		},
+	]
+
+	return (
+		<>
+			<Divider my="8" borderColor="gray.400" />
+
+			<Flex direction="column">
+				{parse &&
+					parse.map((item) => (
+						<Flex
+							direction="row"
+							alignItems="center"
+							bg={item.bg ? item.bg : 'gray.100'}
+							w="100%"
+							p="2"
+							mb="4"
+							borderRadius="md"
+						>
+							<Box flex="1">
+								<Heading fontWeight="normal" size="xs">
+									{item.label}
+								</Heading>
+								<Text fontWeight="semibold">
+									{item.value}
+								</Text>
+							</Box>
+							<IconButton
+								colorScheme="blue"
+								_hover={{ bg: 'blue.100' }}
+								variant="ghost"
+								ml="2"
+								icon={<CopyIcon />}
+								onClick={() =>
+									clickCopy(item.value, item.label)
+								}
+							/>
+						</Flex>
+					))}
+			</Flex>
+		</>
+	)
+}
+
 export default function Home() {
+	const [loading, setLoading] = useState(false)
+
 	const [current, setCurrent] = useState(null)
 	const [newCnpj, setNewCnpj] = useState('')
 
 	async function searchLead(cnpj) {
 		const cnpjSanit = cnpj.trim().replace(/[^0-9]/g, '')
 
+		setCurrent(null)
+		setLoading(true)
 		try {
 			const res = await fetch(
 				`https://publica.cnpj.ws/cnpj/${cnpjSanit}`,
@@ -35,11 +145,12 @@ export default function Home() {
 			if (res.status === 200) {
 				const data = await res.json()
 				setCurrent(data)
-
+				setLoading(false)
 				console.log('CURRENT', current)
 			}
 		} catch (err) {
 			console.log(err)
+			setLoading(false)
 		}
 	}
 
@@ -48,7 +159,7 @@ export default function Home() {
 			<Head>
 				<title>Leads Search - Santiago</title>
 			</Head>
-			<Container bg="white" as="main" pt="16">
+			<Container bg="white" as="main" pt="8" pb="16">
 				<Heading mb="8">Buscar Leads</Heading>
 
 				<FormControl>
@@ -64,6 +175,7 @@ export default function Home() {
 						<Button
 							ml="2"
 							colorScheme="blue"
+							isLoading={loading}
 							onClick={() => searchLead(newCnpj)}
 						>
 							Buscar
@@ -74,101 +186,7 @@ export default function Home() {
 					</FormHelperText>
 				</FormControl>
 
-				{current && (
-					<>
-						<Divider my="8" borderColor="gray.400" />
-
-						<Flex direction="column">
-							<Box
-								bg={
-									current.estabelecimento
-										.situacao_cadastral !== 'Ativa'
-										? 'red.100'
-										: 'green.100'
-								}
-								w="100%"
-								p="2"
-								mb="4"
-							>
-								<Heading fontWeight="normal" size="xs">
-									Situação Cadastral
-								</Heading>
-								<Text fontWeight="semibold">
-									{
-										current.estabelecimento
-											.situacao_cadastral
-									}
-								</Text>
-							</Box>
-
-							<Box bg="gray.100" w="100%" p="2" mb="4">
-								<Heading fontWeight="normal" size="xs">
-									Razão Social
-								</Heading>
-								<Text fontWeight="semibold">
-									{current.razao_social}
-								</Text>
-							</Box>
-
-							<Box bg="gray.100" w="100%" p="2" mb="4">
-								<Heading fontWeight="normal" size="xs">
-									Nome Fantasia
-								</Heading>
-								<Text fontWeight="semibold">
-									{current.estabelecimento.nome_fantasia}
-								</Text>
-							</Box>
-
-							<Box bg="gray.100" w="100%" p="2" mb="4">
-								<Heading fontWeight="normal" size="xs">
-									Cidade - UF
-								</Heading>
-								<Text fontWeight="semibold">
-									{`${current.estabelecimento.cidade.nome} - ${current.estabelecimento.estado.sigla}`}
-								</Text>
-							</Box>
-
-							<Box bg="gray.100" w="100%" p="2" mb="4">
-								<Heading fontWeight="normal" size="xs">
-									E-mail
-								</Heading>
-								<Text fontWeight="semibold">
-									{current.estabelecimento.email}
-								</Text>
-							</Box>
-
-							<Box bg="gray.100" w="100%" p="2" mb="4">
-								<Heading fontWeight="normal" size="xs">
-									Telefones
-								</Heading>
-								<Text fontWeight="semibold">
-									{`(${
-										current.estabelecimento.ddd1
-									}) ${current.estabelecimento.telefone1.replace(
-										/(.{4}$)/,
-										'-$1'
-									)}`}
-									{current.estabelecimento.ddd2 !== null &&
-										`(${
-											current.estabelecimento.ddd2
-										}) ${current.estabelecimento.telefone2.replace(
-											/(.{4}$)/,
-											'-$1'
-										)}`}
-								</Text>
-							</Box>
-
-							<Box bg="gray.100" w="100%" p="2" mb="4">
-								<Heading fontWeight="normal" size="xs">
-									Endereço
-								</Heading>
-								<Text fontWeight="semibold">
-									{`${current.estabelecimento.tipo_logradouro} ${current.estabelecimento.logradouro}, ${current.estabelecimento.numero}, ${current.estabelecimento.cidade.nome}`}
-								</Text>
-							</Box>
-						</Flex>
-					</>
-				)}
+				{current && <Consulta data={current} />}
 			</Container>
 		</>
 	)
